6f58017ca14a4eb3eaa8423f0fa918fdfcd885a9
황원상[투자플랫폼] <wonsang.hwang@navercorp.com>
2022-01-19 22:17:33 +0900

    error code and message

--- src/main/kotlin/com/example/springthymeleafintegration/controller/BasicItemController.kt
+++ src/main/kotlin/com/example/springthymeleafintegration/controller/BasicItemController.kt
@@ -91,7 +91,7 @@ class BasicItemController(private val itemRepository: ItemRepository) {
         bindingResult: BindingResult,
         redirectAttributes: RedirectAttributes
     ): String {
-        if (validationUsingBindingResultV2(item, bindingResult)) return "basic/addForm"
+        if (validationUsingBindingResultV4(item, bindingResult)) return "basic/addForm"
 
         val savedItem = itemRepository.save(item.toEntity())
 
@@ -178,6 +178,105 @@ class BasicItemController(private val itemRepository: ItemRepository) {
                         null,
                         "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = $resultPrice"
                     )
+                )
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        return bindingResult.hasErrors()
+    }
+
+    private fun validationUsingBindingResultV3(item: Item, bindingResult: BindingResult): Boolean {
+        //검증로직
+        if (!StringUtils.hasText(item.itemName)) {
+            bindingResult.addError(
+                FieldError(
+                    "item",
+                    "itemName",
+                    item.itemName,
+                    false,
+                    arrayOf("required.item.itemName"),
+                    null,
+                    null
+                )
+            )
+        }
+
+        if (item.price == null ||
+            item.price < 1000 ||
+            item.price > 1000000
+        ) {
+            bindingResult.addError(
+                FieldError(
+                    "item",
+                    "price",
+                    item.price,
+                    false,
+                    arrayOf("range.item.price"),
+                    arrayOf(1000, 1000000),
+                    null
+                )
+            )
+        }
+
+        if (item.quantity == null || item.quantity >= 9999) {
+            bindingResult.addError(
+                FieldError(
+                    "item",
+                    "quantity",
+                    item.quantity,
+                    false,
+                    arrayOf("max.item.quantity"),
+                    arrayOf(9999),
+                    "수량은 최대 9,999 까지 허용합니다."
+                )
+            )
+        }
+
+        // 특정필드가 아닌 복합필드 검증
+        if (item.price != null && item.quantity != null) {
+            val resultPrice = item.price * item.quantity
+            if (resultPrice < 10000) {
+                bindingResult.addError(
+                    ObjectError(
+                        "item",
+                        arrayOf("totalPriceMin"),
+                        arrayOf(10000, resultPrice),
+                        null
+                    )
+                )
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        return bindingResult.hasErrors()
+    }
+
+    private fun validationUsingBindingResultV4(item: Item, bindingResult: BindingResult): Boolean {
+        //검증로직
+        if (!StringUtils.hasText(item.itemName)) {
+            bindingResult.rejectValue("itemName", "required")
+        }
+
+        if (item.price == null ||
+            item.price < 1000 ||
+            item.price > 1000000
+        ) {
+            bindingResult.rejectValue("price", "range", arrayOf(1000, 1000000), null)
+        }
+
+        if (item.quantity == null || item.quantity >= 9999) {
+            bindingResult.rejectValue("quantity", "max", arrayOf(9999), null)
+        }
+
+        // 특정필드가 아닌 복합필드 검증
+        if (item.price != null && item.quantity != null) {
+            val resultPrice = item.price * item.quantity
+            if (resultPrice < 10000) {
+                bindingResult.reject(
+                    "totalPriceMin",
+                    arrayOf(10000, resultPrice),
+                    null
                 )
             }
         }
