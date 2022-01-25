70989c5436493ee76efcc43ed625afa93a3bcdd2
황원상[투자플랫폼] <wonsang.hwang@navercorp.com>
2022-01-25 16:48:24 +0900

    세션적용하기

--- src/main/kotlin/com/example/springthymeleafintegration/controller/HomeController.kt
+++ src/main/kotlin/com/example/springthymeleafintegration/controller/HomeController.kt
@@ -1,19 +1,26 @@
 package com.example.springthymeleafintegration.controller
 
+import com.example.springthymeleafintegration.domain.Member
 import com.example.springthymeleafintegration.repository.MemberRepository
+import com.example.springthymeleafintegration.session.SessionManager
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.CookieValue
 import org.springframework.web.bind.annotation.GetMapping
+import javax.servlet.http.HttpServletRequest
 
 @Controller
-class HomeController(private val memberRepository: MemberRepository) {
+class HomeController(
+    private val memberRepository: MemberRepository,
+    private val sessionManager: SessionManager
+) {
 
     //    @GetMapping("/")
     @Deprecated("사용하지 않는 함수")
     fun home(): String = "home"
 
-    @GetMapping("/")
+    @Deprecated("")
+    //@GetMapping("/")
     fun homeLogin(
         @CookieValue(name = "memberId", required = false) memberId: Long?,
         model: Model
@@ -26,5 +33,15 @@ class HomeController(private val memberRepository: MemberRepository) {
         model.addAttribute("member", loginMember.get())
         return "loginHome"
 
+    }
+
+    @GetMapping("/")
+    fun homeLoginV2(
+        request: HttpServletRequest,
+        model: Model
+    ): String {
+        val member = sessionManager.getSession(request) as Member? ?: return "home"
+        model.addAttribute("member", member)
+        return "loginHome"
     }
 }
--- src/main/kotlin/com/example/springthymeleafintegration/controller/LoginController.kt
+++ src/main/kotlin/com/example/springthymeleafintegration/controller/LoginController.kt
@@ -3,6 +3,7 @@ package com.example.springthymeleafintegration.controller
 import com.example.springthymeleafintegration.domain.Member
 import com.example.springthymeleafintegration.dto.LoginForm
 import com.example.springthymeleafintegration.service.LoginService
+import com.example.springthymeleafintegration.session.SessionManager
 import mu.KotlinLogging
 import org.springframework.stereotype.Controller
 import org.springframework.validation.BindingResult
@@ -11,10 +12,14 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @Controller
-class LoginController(private val loginService: LoginService) {
+class LoginController(
+    private val loginService: LoginService,
+    private val sessionManager: SessionManager
+) {
 
     private val logger = KotlinLogging.logger {}
 
@@ -23,7 +28,8 @@ class LoginController(private val loginService: LoginService) {
         return "login/loginForm"
     }
 
-    @PostMapping("/login")
+    //    @PostMapping("/login")
+    @Deprecated("사용하지 않음")
     fun login(
         @Validated @ModelAttribute form: LoginForm,
         bindingResult: BindingResult,
@@ -45,6 +51,28 @@ class LoginController(private val loginService: LoginService) {
         return "redirect:/"
     }
 
+    @PostMapping("/login")
+    fun loginV2(
+        @Validated @ModelAttribute form: LoginForm,
+        bindingResult: BindingResult,
+        response: HttpServletResponse
+    ): String {
+        if (bindingResult.hasErrors()) return "login/loginForm"
+
+        val loginMember = loginService.login(form.loginId!!, form.password!!)
+
+        logger.info { "login? $loginMember" }
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "id or password 가 맞지 않습니다.")
+            return "login/loginForm"
+        }
+
+        sessionManager.createSession(loginMember, response)
+
+        return "redirect:/"
+    }
+
     private fun setCookie(
         loginMember: Member,
         response: HttpServletResponse
@@ -53,9 +81,16 @@ class LoginController(private val loginService: LoginService) {
         response.addCookie(cookie)
     }
 
-    @PostMapping("/logout")
+    //@PostMapping("/logout")
+    @Deprecated("사용하지 않음")
     fun logout(response: HttpServletResponse): String {
         expireCookie(response, "memberId")
+        return "redirect:/"
+    }
+
+    @PostMapping("/logout")
+    fun logoutV2(request: HttpServletRequest): String {
+        sessionManager.expire(request)
         return "redirect:/"
     }
 
